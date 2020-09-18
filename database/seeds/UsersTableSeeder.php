4684180d8aad71dfeee57e78aca14bfe9c135b1f
thinh.pham <pst269@gmail.com>
2020-09-18 10:30:37 +0700

    fix(seeder): add admin to user table seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Model\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Role;
 
 class UsersTableSeeder extends Seeder
@@ -14,8 +15,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $roleAdmin = Role::query()->where('name', 'admin')->pluck('id');
+        $roleAdmin  = Role::query()->where('name', 'admin')->pluck('id');
         $roleMember = Role::query()->where('name', 'member')->pluck('id');
+        DB::table('users')->insert([
+            'name'              => 'admin',
+            'email'             => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password'          => \Illuminate\Support\Facades\Hash::make('123456789aA'),
+            'remember_token'    => Str::random(10),
+        ]);
+        User::query()
+            ->where('name', 'admin')
+            ->first()
+            ->assignRole($roleAdmin);
+
         factory(User::class, 2)->create()->each(function ($user) use (
             $roleAdmin,
             $roleMember
@@ -23,7 +36,7 @@ class UsersTableSeeder extends Seeder
             $user->assignRole($roleAdmin);
             $user->assignRole($roleMember);
         });
-        factory(User::class, 3)->create()->each(function ($user) use (
+        factory(User::class, 5)->create()->each(function ($user) use (
             $roleMember
         ) {
             $user->assignRole($roleMember);
